ca9b1d5260c43622555c300473ce6d8fdaf5d98d
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-10-15 05:29:37 +0000

    Update babyvideo.js

--- scripts/commands/babyvideo.js
+++ scripts/commands/babyvideo.js
@@ -20,7 +20,7 @@ module.exports.run = async ({ api, event, args, client, Users, Threads, __GLOBAL
   const request = global.nodemodule["request"];
   const fs = global.nodemodule["fs-extra"];
 
-  var hi = ["☞ masum baby video ❉"];
+  var hi = ["-🔰 -♦𝗕Ø𝗦𝗦 𝗧𝗔𝗡𝗩𝗜𝗥♦- 🔰-"];
   var know = hi[Math.floor(Math.random() * hi.length)];
 
   var link = [
