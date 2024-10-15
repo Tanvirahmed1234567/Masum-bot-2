e1ca7fabc5e81566bb02cd4478743402050b7e43
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-10-15 07:42:25 +0000

    Update npx4.js

--- scripts/commands/npx4.js
+++ scripts/commands/npx4.js
@@ -1,31 +1,40 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "npx4",
+  name: "npx4",
         version: "1.0.1",
         prefix: false,
-	permssion: 0,
-	credits: "Fahim", 
-	description: "Fun",
-	category: "no prefix",
-	usages: "",
+  permssion: 0,
+  credits: "nayan", 
+  description: "Fun",
+  category: "no prefix",
+  usages: "😒",
         cooldowns: 5, 
 },
 
-handleEvent: function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
+handleEvent: async function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf("🕵️‍♀️")==0 || body.indexOf("😜")==0 || body.indexOf(" ")==0) {
-		var msg = {
-				body: "জা্ঁন্ঁ প্ঁম‌্ঁ'প্ঁম্ঁ খা্ঁবা্ঁ__🤭🤤",
-				attachment: fs.createReadStream(__dirname + `/ArYan/popom khaiba.m4a`)
-			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🙊", event.messageID, (err) => {}, true)
-		}
-	},
-	start: function({ nayan }) {
+  const axios = require('axios')
+  const NAYAN = ["https://i.imgur.com/64RETlb.mp4","https://i.imgur.com/wbpQrKu.mp4","https://i.imgur.com/DL7kpJc.mp4","https://i.imgur.com/FNxOc4F.mp4","https://i.imgur.com/nJo4WH1.mp4","https://i.imgur.com/3oh3Yiq.mp4","https://i.imgur.com/G2UfW7m.mp4",'https://i.imgur.com/SNhYQi2.mp4','https://i.imgur.com/wkz6F6D.mp4','https://i.imgur.com/oLTkb9h.mp4','https://i.imgur.com/WLs3gp6.mp4','https://i.imgur.com/CPFD7Gm.mp4']
+    var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
+const media = (
+    await axios.get(
+      `${rndm}`,
+      { responseType: 'stream' }
+    )
+  ).data;
 
-  }
+  if (body.indexOf("🖤")==0 || body.indexOf("🤍")==0 || body.indexOf("❤️")==0 || body.indexOf("💛")==0 || body.indexOf("💚")==0 || body.indexOf("🩵")==0 || body.indexOf("💙")==0 || body.indexOf("💖")==0 || body.indexOf("💘")==0 || body.indexOf("💝")==0) {
+    var msg = {
+        body: "-♦𝗕Ø𝗦𝗦 𝗧𝗔𝗡𝗩𝗜𝗥♦-\n🔰___|•সঠিক মানুষের চোখে তুমি সব সময় সুন্দর!_//-🤍🌸",
+        attachment: media
       }
+      api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😇", event.messageID, (err) => {}, true)
+    }
+  },
+  start: function({ nayan }) {
+  }
+}
