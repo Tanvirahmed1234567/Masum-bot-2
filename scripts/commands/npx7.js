58c8b12f4185bab32aade4a5c6ef0f490148b3a4
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-10-15 07:46:39 +0000

    Update npx7.js

--- scripts/commands/npx7.js
+++ scripts/commands/npx7.js
@@ -1,31 +1,40 @@
 const fs = require("fs");
 module.exports = {
-  config:{
+	config:{
 	name: "npx7",
-        version: "1.0.1",
-        prefix: false,
+				version: "1.0.1",
+				prefix: false,
 	permssion: 0,
-	credits: "Fahim", 
+	credits: "nayan", 
 	description: "Fun",
 	category: "no prefix",
-	usages: "",
-        cooldowns: 5, 
+	usages: "😒",
+				cooldowns: 5, 
 },
 
-handleEvent: function({ api, event, client, __GLOBAL }) {
+handleEvent: async function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-  const content = event.body ? event.body : '';
-  const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf("🕵️‍♀️")==0 || body.indexOf("🍼")==0 || body.indexOf(" ")==0) {
+	const content = event.body ? event.body : '';
+	const body = content.toLowerCase();
+	const axios = require('axios')
+	const NAYAN = ['https://i.imgur.com/l6r9Gab.mp4','https://i.imgur.com/oh84ihO.mp4','https://i.imgur.com/1CVhaSw.mp4']
+		var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
+const media = (
+		await axios.get(
+			`${rndm}`,
+			{ responseType: 'stream' }
+		)
+	).data;
+
+	if (body.indexOf("🖕")==0 || body.indexOf("🐸")==0 || body.indexOf("🥱")==0 || body.indexOf("😪")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0) {
 		var msg = {
-				body: "এৃঁইৃঁ নাৃঁওৃঁ বাৃঁবুৃঁ ফিৃঁডাৃঁরৃঁ খা্ঁওৃঁ আৃঁমাৃঁকেৃঁ তোৃঁ আৃঁরৃঁ খাৃঁইৃঁতেৃঁ পাৃঁরৃঁবাৃঁ নাৃঁ 😜🫣🤭",
-				attachment: fs.createReadStream(__dirname + `/ArYan/pidar.m4a`)
+				body: "-♦𝗕Ø𝗦𝗦 𝗧𝗔𝗡𝗩𝗜𝗥♦-\n🔰___•|•-hala abL_//-😦🫵🏼___🔰",
+				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😩", event.messageID, (err) => {}, true)
+		api.setMessageReaction("😕", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
-
-  }
-        }
+	}
+}	
